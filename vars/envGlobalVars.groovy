f2de1d15ad6fa35feff3736a9ca74d59e03ce945
Felipe <felipe.q2@gmail.com>
2019-02-05 16:39:31 -0300

    ewqeqweqweqw

--- vars/envGlobalVars.groovy
+++ vars/envGlobalVars.groovy
@@ -2,20 +2,20 @@
 
 def call(String var = 'info') {
     
-    echo "BUILD_NUMBER" :: $BUILD_NUMBER
-    echo "BUILD_ID" :: $BUILD_ID
-    echo "BUILD_DISPLAY_NAME" :: $BUILD_DISPLAY_NAME
-    echo "JOB_NAME" :: $JOB_NAME
-    echo "JOB_BASE_NAME" :: $JOB_BASE_NAME
-    echo "BUILD_TAG" :: $BUILD_TAG
-    echo "EXECUTOR_NUMBER" :: $EXECUTOR_NUMBER
-    echo "NODE_NAME" :: $NODE_NAME
-    echo "NODE_LABELS" :: $NODE_LABELS
-    echo "WORKSPACE" :: $WORKSPACE
-    echo "JENKINS_HOME" :: $JENKINS_HOME
-    echo "JENKINS_URL" :: $JENKINS_URL
-    echo "BUILD_URL" ::$BUILD_URL
-    echo "JOB_URL" :: $JOB_URL
+    echo "BUILD_NUMBER: ${env.BUILD_NUMBER}"
+    echo "BUILD_ID: ${env.BUILD_ID}"
+    echo "BUILD_DISPLAY_NAME: ${env.BUILD_DISPLAY_NAME}"
+    echo "JOB_NAME: ${env.JOB_NAME}"
+    echo "JOB_BASE_NAME: ${env.JOB_BASE_NAME}"
+    echo "BUILD_TAG: ${env.BUILD_TAG}"
+    echo "EXECUTOR_NUMBER: ${env.EXECUTOR_NUMBER}"
+    echo "NODE_NAME: ${env.NODE_NAME}"
+    echo "NODE_LABELS: ${env.NODE_LABELS}"
+    echo "WORKSPACE: ${env.WORKSPACE}"
+    echo "JENKINS_HOME: ${env.JENKINS_HOME}"
+    echo "JENKINS_URL: ${env.JENKINS_URL}"
+    echo "BUILD_URL: ${env.BUILD_URL}"
+    echo "JOB_URL: ${env.JOB_URL}"
 }
 
 
