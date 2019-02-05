0c9cf37ecc71abaa9f3a17ba1da8ec8ad3ba221f
Felipe <felipe.q2@gmail.com>
2019-02-05 16:25:57 -0300

    adsdadsadsa

--- vars/infoJob.groovy
+++ vars/infoJob.groovy
@@ -1,8 +1,9 @@
 #!/bin/env groovy
 
-def call() {
+def call(String name = 'human') {
 
     //Simple sample
     echo "Job: '${env.JOB_NAME}' [${env.BUILD_NUMBER}]"
     echo "Git User: '${env.GIT_AUTHOR_NAME}' [${env.GIT_COMMITTER_EMAIL}]"  
+    echo "Hello, ${name}"
 }
