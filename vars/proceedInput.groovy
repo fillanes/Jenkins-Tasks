f8d3cd8d260568aaaeeac6655930beed7cd3dfdb
Felipe <felipe.q2@gmail.com>
2019-02-06 11:50:23 -0300

    add src folder

---
+++ vars/proceedInput.groovy
@@ -0,0 +1,14 @@
+
+def call(String submitterID = 'accountID'){
+
+    def userInput
+    try {
+        userInput = input(id: 'Proceed1', message: 'Desea pasar a Producción?', submitter: "${submitterID}")
+        echo "userInput = [${userInput}]"
+    } catch(err) { // input false
+        def user = err.getCauses()[0].getUser() 
+        echo "Abortado por: [${user}]"
+        currentBuild.result = 'ABORTED'
+    }
+}
+
